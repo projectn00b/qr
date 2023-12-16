b5df9a2bf71e28bec2797307b98a2bfa2549833f
Matthew James Slaton <matthewslaton82@gmail.com>
2023-12-16 11:55:09 -0500

    update

--- main.py
+++ main.py
@@ -74,4 +74,4 @@ def favicon():
     return send_from_directory(app._static_folder, 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080)  # Change port to the desired value
